1d904804e880b8cfc84ccb05def074fadbd91553
Dan Hett <hellodanhett@gmail.com>
2016-04-25 10:52:17 +0100

    Added KICK SNARE and HIHAT to audio gen

--- js/grammars.js
+++ js/grammars.js
@@ -23,7 +23,7 @@ exports.grammars = {
         numlarge : ["50", "60", "70", "100", "120", "140", "160", "200", "220", "500"],
         numrand : ["1", "2", "3", "4", "5", "10", "20", "50", "100"],
         neg : ["", "-"],
-        audio : ["", " * #randaudio#"],
+        audio : ["", " + (#nummed# * #randaudio#)", " + (#nummed# * KICK)", " + (#nummed# * SNARE)", " + (#nummed# * HIHAT)"],
         randaudio: ["fft(#fft#)"],
         fft: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
         geom: ["box #nummed#", "box #nummed#, #nummed#, #nummed#", "peg #nummed#", "sphere #nummed#"]
