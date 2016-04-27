6f842e5fafc5e5d8fbf70304d4ac7812ac25e5e9
Dan Hett <hellodanhett@gmail.com>
2016-04-27 20:38:42 +0100

    Added some notes for editing

--- js/generator.js
+++ js/generator.js
@@ -7,6 +7,7 @@ grammar.addModifiers(tracery.baseEngModifiers);
 
 var looping = false;
 var timer;
+var output;
 
 /**
  * Generates a single pattern, no loops.
@@ -24,9 +25,27 @@ exports.startLoop = function() {
     console.log("Starting loop...");
 
     generatePattern();
-    timer = setInterval(generatePattern, 5000);
+
+    analyseChangableElements();
+    //timer = setInterval(generatePattern, 5000);
 
     looping = true;
+}
+
+/**
+ * Identifies numbers, params and other elements that can
+ * be changed while the editor process is running. Assembles
+ * a list in advance, randomises it, and works through.
+ */
+exports.analyseChangableElements = function() {
+    // TODO - find and assemble list of candidate numbers and colours etc
+    // TODO - push into list, and provide a randomised series of possibles for each
+    // TODO - start a timer, and work through the list, editing as we go
+    // TODO - at a given threshold, regenerate the imagery entirely and restart this process
+    // TODO - at every stage, assess if the image is likely to be a coloured screen
+    //        OR could we somehow actually analyse the screen's content??? How?
+
+
 }
 
 /**
@@ -47,9 +66,10 @@ exports.stopLoop = function() {
  * Actual pattern generation, used in both modes.
  */
 var generatePattern = function() {
-    var output = grammar.flatten('#origin#');
+    output = grammar.flatten('#origin#');
     console.log(output);
 
+    // TODO - only write to 0.cy or can we kick Cyril into using multiple?
     fs.writeFile('data/code/0.cy', output, function (err) {
       if (err) return console.log(err);
     });
